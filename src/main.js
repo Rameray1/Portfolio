66de00d7378aefb55133eba1f6cfa7e07e2e47c8
Ramazan Eray Kus <ramazan.kus@etu.univ-lyon1.fr>
2024-06-18 21:17:37 +0200

    aj

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import './assets/HomeView.css';
 import './assets/AboutView.css';
 import './assets/ProjectsView.css';
 
+document.title = `Ramazan KUS`;
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
+
